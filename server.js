4ec452321f0bb14baca96f165362f45cf3eab17b
Ivy Do <ivy.do2011@gmail.com>
2022-03-07 06:17:17 -0500

    heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 const apiRoutes = require("./routes/api.js");
+const connection_url = "mongodb+srv://ivydo:796!AQnp-ZjXuT2@cluster0.quy5e.mongodb.net/budgetDB"
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,9 +15,9 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("/public"));
 
-const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget" || connection_url;
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useFindAndModify: false,
